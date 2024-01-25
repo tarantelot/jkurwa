275cfeb34c05b1c0fd75a0f35291c4e934c6fe38
tarantelot <tarantelot@gmail.com>
2024-01-25 15:47:16 +0200

    modified | password check for browser

--- examples/is_password_correct.js
+++ examples/is_password_correct.js
@@ -6,14 +6,13 @@ const key_path     = ""
 const key_password = ''
 
 try {
-    const fileContent = fs.readFileSync(key_path);
+    const buff = fs.readFileSync(key_path);
   
-
-    if (isPasswordCorrect(fileContent, key_password)) {
+    if (isPasswordCorrect(buff, key_password)){
       console.log('Password is correct!');
     } else {
       console.log('Password is incorrect!');
     }
-  } catch (err) {
-    console.error('Error reading file:', err);
-  }
+} catch (err) {
+  console.error('Error reading file:', err);
+}
